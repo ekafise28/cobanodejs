04529f47ebf5f0f15b07c1f435639c752b8aec47
ekafise <ekafise.st@gmail.com>
2019-03-28 11:11:21 +0700

    coba mongoose

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const mongoose = require("mongoose");
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
@@ -8,3 +9,14 @@ express()
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+  
+var mongoDB = "mongodb://heroku_login123:Ayam!123@ds123196.mlab.com:23196/heroku_qgkt37l7";
+
+mongoose.connect(mongoDB, {
+  useNewUrlParser: true
+});
+
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
