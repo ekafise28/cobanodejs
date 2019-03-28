27c08aa0f3931d27002763a6cea5c56d7702c766
ekafise <ekafise.st@gmail.com>
2019-03-28 12:05:47 +0700

    masukan lagi dta dari sana

---
+++ public/js/kotakab.js
@@ -0,0 +1,58 @@
+$(function(){
+
+   // load_json_data('province');
+    function load_json_data(nameid, provinceid)
+    {
+     var html_code = '';
+    //wa-jib
+     var getJSON = function(url, callback) {
+
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', url, true);
+        xhr.responseType = 'json';
+        
+        xhr.onload = function() {
+        
+            var status = xhr.status;
+            
+            if (status == 200) {
+                callback(null, xhr.response);
+            } else {
+                callback(status);
+            }
+        };        
+        xhr.send();
+    
+        };
+    
+        getJSON('http://localhost:3000/admin/products/kotakabjson',  function(err, data) {
+        
+        html_code += '<option value="">Select '+ nameid +'</option>';
+          $.each(data, function(key, value){
+        
+             if(value.province == provinceid)
+             {
+              html_code += '<option value="'+value.name+'">'+value.name+'</option>';
+             }
+           
+          });
+          $('#kota_kab').html(html_code);
+         });
+    }   
+    
+        $(document).on('change', '#province', function(){
+            var province = $(this).val();
+            if(province != '')
+            {
+             load_json_data('name', province);
+            }
+            else
+            {
+             $('#name').html('<option value="">Select city</option>');
+            }
+    });
+   
+    
+   
+
+});
